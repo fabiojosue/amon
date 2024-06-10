8f3d0db69dac30e2442d37b484705bfdaa37e1cb
RichLeonC <r29leonc@gmail.com>
2024-06-10 17:31:23 -0600

    Update service.service.ts

--- Frontend/amon-app/src/app/service/service.service.ts
+++ Frontend/amon-app/src/app/service/service.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ServiceService {
-  private apiUrl = 'http://localhost:3000';
+  private apiUrl = 'http://localhost:3000/api';
 
   constructor(private http: HttpClient) { }
 
